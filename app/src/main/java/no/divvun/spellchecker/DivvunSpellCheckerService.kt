8fbcb4fb8fe679494e83d300ac8b39990963c66e
mrsata <liuhanrick@gmail.com>
2019-07-11 16:11:34 -0400

    Add TensorFlow Lite example smartreply

--- app/src/main/java/no/divvun/spellchecker/DivvunSpellCheckerService.kt
+++ app/src/main/java/no/divvun/spellchecker/DivvunSpellCheckerService.kt
@@ -9,6 +9,7 @@ import android.view.textservice.TextInfo
 import no.divvun.DivvunSpell
 import no.divvun.DivvunUtils
 import no.divvun.createTag
+import no.nplm.SmartReply
 import java.util.*
 
 class DivvunSpellCheckerService: SpellCheckerService(){
@@ -27,14 +28,17 @@ class DivvunSpellCheckerService: SpellCheckerService(){
     class DivvunSpellCheckerSession(private val context: Context): Session() {
         private val tag = javaClass.simpleName!!
         private var speller: DivvunSpell? = null
+        private var smartReply: SmartReply? = null
 
         override fun onCreate() {
             Log.d(tag, "onCreate")
             speller = DivvunUtils.getSpeller(context, Locale(locale))
+            smartReply = SmartReply(context);
         }
 
         override fun onGetSuggestions(textInfo: TextInfo?, suggestionsLimit: Int): SuggestionsInfo {
             val speller = this.speller ?: return SuggestionsInfo(SuggestionsInfo.RESULT_ATTR_IN_THE_DICTIONARY, arrayOfNulls(0))
+            val smartReply = this.smartReply ?: return SuggestionsInfo(SuggestionsInfo.RESULT_ATTR_IN_THE_DICTIONARY, arrayOfNulls(0))
 
             Log.d(tag, "onGetSuggestions()")
 
@@ -48,10 +52,12 @@ class DivvunSpellCheckerService: SpellCheckerService(){
                 return SuggestionsInfo(SuggestionsInfo.RESULT_ATTR_IN_THE_DICTIONARY, arrayOfNulls(0))
             }
             // If the word isn't correct, query for suggestions
-            val suggestions = speller.suggest(word)
+            // val suggestions = speller.suggest(word)
+            val suggestions = smartReply.suggest(word)
 
             val attrs = SuggestionsInfo.RESULT_ATTR_LOOKS_LIKE_TYPO
-            return SuggestionsInfo(attrs, suggestions.toTypedArray())
+            // return SuggestionsInfo(attrs, suggestions.toTypedArray())
+            return SuggestionsInfo(attrs, suggestions)
         }
 
         override fun onGetSentenceSuggestionsMultiple(textInfos: Array<out TextInfo>?, suggestionsLimit: Int): Array<SentenceSuggestionsInfo> {
---
+++ app/src/main/java/no/nplm/SmartReply.java
@@ -0,0 +1,60 @@
+package no.nplm;
+
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.tensorflow.lite.Interpreter;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+
+
+public class SmartReply {
+
+    private static final String TAG = "SmartReply";
+    private static final String MODEL_PATH = "smartreply.tflite";
+
+    private Context context;
+    private MappedByteBuffer model;
+    private Interpreter interpreter;
+
+    public SmartReply(Context context) {
+        try {
+            this.context = context;
+            this.model = loadModelFile();
+            this.interpreter = new Interpreter(this.model);
+        } catch (IOException e) {
+            Log.e(TAG, "Fail to load model", e);
+            return;
+        }
+    }
+
+    public String[] suggest(String word) {
+        String[] placeholderArray = {"placeholder", "is", "wise"};
+        String placeholder = TextUtils.join(",", placeholderArray);
+        ByteBuffer output = Charset.forName("UTF-8").encode(placeholder);
+        ByteBuffer input = Charset.forName("UTF-8").encode(word);
+        this.interpreter.run(input, output);
+        String suggestion = Charset.forName("UTF-8").decode(output).toString();
+        String[] suggestionArray = TextUtils.split(suggestion, ",");
+        return suggestionArray;
+    }
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+        try (AssetFileDescriptor fileDescriptor = context.getAssets().openFd(MODEL_PATH);
+             FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor())) {
+            FileChannel fileChannel = inputStream.getChannel();
+            long startOffset = fileDescriptor.getStartOffset();
+            long declaredLength = fileDescriptor.getDeclaredLength();
+            return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+        }
+    }
+
+
+}
